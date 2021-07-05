b8d6d2e3ee86e99cf7e7455f549cf84ec2e08d3a
Devang <devngc@gmail.com>
2021-07-05 22:40:36 +0530

    chore(*): add vtk logo

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/honeybee-vtk',
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_bugs/grasshopper_tabs/Honeybee.png',
+        'icon': 'assets/logo/vtk.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-vtk'
     },
     description='Honeybee vtk plugin for Pollination.',
