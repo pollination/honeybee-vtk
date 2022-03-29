88e617508df2916169e9ee41034de1b113ba5d6b
Devang <devngc@gmail.com>
2022-03-28 20:24:16 -0400

    chore(*): rename modules

--- tests/export_images_test.py
+++
@@ -1,8 +0,0 @@
-from pollination.honeybee_vtk.export_images import ExportImages
-from queenbee.plugin.function import Function
-
-
-def test_export_images():
-    function = ExportImages().queenbee
-    assert function.name == 'export-images'
-    assert isinstance(function, Function)
