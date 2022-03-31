d7a789cbfca006f86b2645cb0b8d613b5f508a5f
Devang <devngc@gmail.com>
2022-03-31 15:11:26 -0400

    refactor(*): move time-step-data to config and remove config function

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,8 +1,8 @@
-from pollination.honeybee_vtk.config import Config
+from pollination.honeybee_vtk.config import TimeStepData
 from queenbee.plugin.function import Function
 
 
-def test_write_config():
-    function = Config().queenbee
-    assert function.name == 'config'
+def test_time_step_data():
+    function = TimeStepData().queenbee
+    assert function.name == 'time-step-data'
     assert isinstance(function, Function)
--- tests/time_step_data_test.py
+++
@@ -1,8 +0,0 @@
-from pollination.honeybee_vtk.time_step_data import TimeStepData
-from queenbee.plugin.function import Function
-
-
-def test_time_step_data():
-    function = TimeStepData().queenbee
-    assert function.name == 'time-step-data'
-    assert isinstance(function, Function)
