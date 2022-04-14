ac10edf3915fcef9157ddd22803b0055b6efe0a6
mostaphaRoudsari <sadeghipour@gmail.com>
2022-04-14 15:11:12 -0400

    fix(translate): expose grid display mode

--- pollination/honeybee_vtk/translate.py
+++ pollination/honeybee_vtk/translate.py
@@ -23,10 +23,18 @@ class Translate(Function):
               'enum': ['vtkjs', 'vtp', 'vtk']}
     )
 
-    display_mode = Inputs.str(
+    model_display_mode = Inputs.str(
         description='Set display mode for the model.'
         ' Choose from: shaded, surface, surfacewithedges, wireframe, points.',
         default='shaded',
+        spec={'type': 'string',
+              'enum': ['shaded', 'surface', 'surfacewithedges', 'wireframe', 'points']}
+    )
+
+    grid_display_mode = Inputs.str(
+        description='Set display mode for the sensor grids.'
+        ' Choose from: shaded, surface, surfacewithedges, wireframe, points.',
+        default='surfacewithedges',
         spec={'type': 'string',
               'enum': ['shaded', 'surface', 'surfacewithedges', 'wireframe', 'points']}
     )
@@ -57,9 +65,10 @@ class Translate(Function):
     @command
     def translate_model(self):
         return 'honeybee-vtk translate --name {{self.name}} --file-type' \
-            ' {{self.file_type}} --model-display-mode {{self.display_mode}}' \
-            ' --grid-options {{self.grid_options}} '\
-            '--triangle-angle {{self.triangle_angle}}' \
+            ' {{self.file_type}} --model-display-mode {{self.model_display_mode}}' \
+            ' --grid-display-mode {{self.grid_display_mode}}' \
+            ' --grid-options {{self.grid_options}}'\
+            ' --triangle-angle {{self.triangle_angle}}' \
             ' --triangle-radius {{self.triangle_radius}}'\
             ' --config input_data/config.json input.hbjson --folder target_folder'
 
